40680d6976a64928f978ccc2fa8981f45297ac4c
khwilo <khwilowatai@gmail.com>
2019-06-04 14:50:06 +0300

    Create the sign-up request payload

---
+++ src/main/java/com/khwilo/employee_manager/payload/SignUpRequest.java
@@ -0,0 +1,56 @@
+package com.khwilo.employee_manager.payload;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 4, max = 40)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 4, max = 40)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String emailAddress;
+
+    @NotBlank
+    @Size(min = 8, max = 40)
+    private String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
