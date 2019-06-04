861c9291546b601da8ecda88f7b029cc70e69e34
khwilo <khwilowatai@gmail.com>
2019-06-04 15:29:10 +0300

    Seed the database with an admin on application start up

--- src/main/java/com/khwilo/employee_manager/EmployeeManagerApplication.java
+++ src/main/java/com/khwilo/employee_manager/EmployeeManagerApplication.java
@@ -1,13 +1,28 @@
 package com.khwilo.employee_manager;
 
+import com.khwilo.employee_manager.model.Employee;
+import com.khwilo.employee_manager.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeManagerApplication {
+public class EmployeeManagerApplication implements ApplicationRunner {
+    @Autowired
+    EmployeeService employeeService;
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagerApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Employee employee = new Employee(
+                "Frank", "Sinatra", "frank@gmail.com", "sinatra@1234"
+        );
+        employee.setAdmin(true);
+        employeeService.save(employee);
+    }
 }
