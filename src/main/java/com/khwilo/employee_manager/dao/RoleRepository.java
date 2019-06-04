1f3e2a1d1d55096ef9090e1944e2878ce941c447
khwilo <khwilowatai@gmail.com>
2019-06-04 22:38:41 +0300

    Remove the Role repository

--- src/main/java/com/khwilo/employee_manager/dao/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.khwilo.employee_manager.dao;
-
-import com.khwilo.employee_manager.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
