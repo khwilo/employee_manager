4f1e200562c408bcf24726b99ddab61de0ecf1a0
khwilo <khwilowatai@gmail.com>
2019-06-04 15:24:11 +0300

    Create the class for the sample data

---
+++ src/test/java/com/khwilo/employee_manager/utility/SampleData.java
@@ -0,0 +1,16 @@
+package com.khwilo.employee_manager.utility;
+
+import com.google.gson.JsonObject;
+
+public class SampleData {
+
+    public static String createEmployee(String firstName, String lastName, String emailAddress, String password) {
+        JsonObject employee = new JsonObject();
+        employee.addProperty("firstName", firstName);
+        employee.addProperty("lastName", lastName);
+        employee.addProperty("emailAddress", emailAddress);
+        employee.addProperty("password", password);
+
+        return employee.toString();
+    }
+}
