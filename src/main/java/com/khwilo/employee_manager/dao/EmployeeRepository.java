02b1b90bdd759f374521560dddb5052b519c29ad
khwilo <khwilowatai@gmail.com>
2019-06-04 16:13:52 +0300

    Define the method to check if an employee exists by email and password

--- src/main/java/com/khwilo/employee_manager/dao/EmployeeRepository.java
+++ src/main/java/com/khwilo/employee_manager/dao/EmployeeRepository.java
@@ -4,11 +4,8 @@ import com.khwilo.employee_manager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Boolean existsEmployeeById(Long id);
     Boolean existsByEmailAddress(String emailAddress);
-    Optional<Employee> findEmployeeByEmailAddress(String emailAddress);
+    Boolean existsByEmailAddressAndPassword(String emailAddress, String password);
 }
