a9384cacef33368be2338b003b6eaf72332dd6e0
khwilo <khwilowatai@gmail.com>
2019-06-05 13:23:39 +0300

    Define method to find an employee by its email address

--- src/main/java/com/khwilo/employee_manager/dao/EmployeeRepository.java
+++ src/main/java/com/khwilo/employee_manager/dao/EmployeeRepository.java
@@ -4,8 +4,11 @@ import com.khwilo.employee_manager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Boolean existsByEmailAddress(String emailAddress);
     Boolean existsByEmailAddressAndPassword(String emailAddress, String password);
+    Optional<Employee> getByEmailAddress(String emailAddress);
 }
